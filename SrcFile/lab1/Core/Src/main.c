b1c5bb991ab9ff86ae5044dca22b53be22c2d417
thanhluan2505 <luan.bui2505@hcmut.edu.vn>
2023-10-05 14:04:58 +0700

    update 4.6

--- SrcFile/lab1/Core/Src/main.c
+++ SrcFile/lab1/Core/Src/main.c
@@ -47,6 +47,7 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -83,14 +84,71 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int n = 0;
   while (1)
   {
+	  switch(n){
+	  	case 0:
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15,0);
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4,1);
+	  		break;
+	  	case 1:
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4,0);
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5,1);
+	  		break;
+	  	case 2:
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5,0);
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6,1);
+	  		break;
+	  	case 3:
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6,0);
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7,1);
+	  		break;
+	  	case 4:
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7,0);
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8,1);
+	  		break;
+	  	case 5:
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8,0);
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9,1);
+	  		break;
+	  	case 6:
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9,0);
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10,1);
+	  		break;
+	  	case 7:
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10,0);
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11,1);
+	  		break;
+	  	case 8:
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11,0);
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12,1);
+	  		break;
+	  	case 9:
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12,0);
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13,1);
+	  		break;
+	  	case 10:
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13,0);
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14,1);
+	  		break;
+	  	case 11:
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14,0);
+	  	 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15,1);
+	  		break;
+	  	default:
+	  		break;
+	  	}
+	  n++;
+	  if(n >=12) n = 0;
+	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -131,6 +189,36 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
+                          |GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA4 PA5 PA6 PA7
+                           PA8 PA9 PA10 PA11
+                           PA12 PA13 PA14 PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
+                          |GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
